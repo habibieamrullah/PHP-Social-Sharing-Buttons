51a026497e94b6ac1fa42412df968b877dbb264c
Habibie <habibieamrullah@gmail.com>
2020-08-09 22:00:53 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//Sharing buttons powered by https://sharingbuttons.io/
+include("sharingbuttons.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>PHP Social Sharing Buttons</title>
+		<link rel="stylesheet" type="text/css" href="https://bazar.applulbayt.web.id/sharingbuttons.css"/>
+	</head>
+	<body>
+		<h1>PHP Social Sharing Buttons</h1>
+		<p>How to use? Call this function: showSharer(string $url, string $message). First paramater is the url that you want to share, second parameter is your message along that url.</p>
+		
+		<?php
+		showSharer("https://google.com/", "A search engine site!");
+		?>
+		
+	</body>
+</html>
